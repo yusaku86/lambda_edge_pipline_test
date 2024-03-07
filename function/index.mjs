68b9ab94db36e6d5edebab0d4d2ad0f15a0b3f5c
yusaku.suzuki <yusaku.suzuki@coosy.co.jp>
2024-03-07 20:00:02 +0900

    :zap: 設定ファイルからリダイレクトを実行する機能追加

--- function/index.mjs
+++ function/index.mjs
@@ -1,4 +1,15 @@
 'use strict';
+const fs = require('fs');
+
+// リダイレクトルールのファイル読み込み
+const redirectRules = JSON.parse(fs.readFileSync('redirect-config.json', 'utf8'));
+
+// ステータスメッセージ
+const statusMessage = {
+    "301": "Moved Permanently",
+    "302": "Found"
+}
+
 export function handler(event, context, callback) {
     const request = event.Records[0].cf.request;
     const uri = request.uri;
@@ -22,10 +33,25 @@ export function handler(event, context, callback) {
         };
         callback(null, response);
     }
+    // リダイレクトルールに含まれるか
+    else if(redirectRules[uri]) {
+        const statuscode = redirectRules[uri].statuscode.toString();
+
+        const response = {
+            status: statuscode,
+            statusDescription: statusMessage[statuscode],
+            headers: {
+                'location': [{
+                    key: 'Location',
+                    value: redirectRules[uri].to
+                }]
+            }
+        };
+        callback(null, response);
+    }
     // URLの末尾が / で終わる場合、/index.htmlを参照する
     else if (uri.endsWith('/')) {
         request.uri += 'index.html';
-
         callback(null, request);
     } else {
         callback(null, request);
