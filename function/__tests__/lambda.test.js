52e0702555b87e4054b2ed56cfae993b06582923
yusaku.suzuki <yusaku.suzuki@coosy.co.jp>
2024-03-08 20:51:54 +0900

    :pencil2: testコード修正

--- function/__tests__/lambda.test.js
+++ function/__tests__/lambda.test.js
@@ -46,8 +46,8 @@ describe('リダイレクト用Lambdaの単体テスト', () => {
     // 1対1のリダイレクトルールに含まれるURLがリダイレクトされるか
     console.log(redirectRule);
     const accessUri = Object.keys(redirectRule)[0];
-    const destinationUri = redirectRule[accessUri][to];
-    const statuscode = redirectRule[accessUri][statuscode].toString();
+    const destinationUri = redirectRule[accessUri].to;
+    const statuscode = redirectRule[accessUri].statuscode.toString();
 
     test('リダイレクトルールに含まれるURLが正しくリダイレクトされるか', () => {
         const callback = (error, response) => {
@@ -61,8 +61,8 @@ describe('リダイレクト用Lambdaの単体テスト', () => {
 
     // 正規表現のリダイレクトルールに含まれるURLがリダイレクトされるか
     const regexAccessUri = Object.keys(regexRedirectRule)[0];
-    const replace = regexRedirectRule[regexAccessUri][to];
-    const regexStatuscode = regexRedirectRule[regexAccessUri][statuscode];
+    const replace = regexRedirectRule[regexAccessUri].to;
+    const regexStatuscode = regexRedirectRule[regexAccessUri].statuscode;
 
     test('正規表現を使用したリダイレクトルールに含まれるURLが正しくリダイレクトされるか', () => {
         const regex = new RegExp(regexAccessUri);
